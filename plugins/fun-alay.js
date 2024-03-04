23c8a45a402eba94759c613c6f126930d49e9bfb
Tanaka <projectthenext1532@gmail.com>
2024-03-05 05:54:25 +0700

    Update fun-alay.js

--- plugins/fun-alay.js
+++ plugins/fun-alay.js
@@ -1,21 +1,41 @@
-function handler(m, { text }) {
-    let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
-    m.reply(teks.replace(/[a-z]/gi, v => Math.random() > .5 ? v[['toLowerCase', 'toUpperCase'][Math.floor(Math.random() * 2)]]() : v).replace(/[abegiors]/gi, v => {
-        if (Math.random() > .5) return v
-        switch (v.toLowerCase()) {
-            case 'a': return '4'
-            case 'b': return Math.random() > .5 ? '8' : '13'
-            case 'e': return '3'
-            case 'g': return Math.random() > .5 ? '6' : '9'
-            case 'i': return '1'
-            case 'o': return '0'
-            case 'r': return '12'
-            case 's': return '5'
-        }
-    }))
-}
-handler.help = ['alay']
-handler.tags = ['fun']
-handler.command = ['alay']
+let handler = async(m, {conn, command, usedPrefix, text}) => {
+  global.db.data.users[m.sender].catatan = global.db.data.users[m.sender].catatan || []
+  let i = 0
+  if (global.db.data.users[m.sender].catatan.length == 0) return m.reply('Kamu belum punya catatan!')
+  let txt = '🗒️Daftar catatan🗒️\n\n'
+  for (let ct in global.db.data.users[m.sender].catatan) {
+    i += 1
+    txt += '[' + i + ']. ' + global.db.data.users[m.sender].catatan[ct].title + '\n'
+  }
+  txt += `\nPenggunaan: ${usedPrefix}hapuscatatan 1`
+  if (text.length == 0) return m.reply(txt)
+  let catatan = global.db.data.users[m.sender].catatan
+  let split = text.split('|')
+  if (catatan.length == 0) return m.reply('Kamu belum memiliki catatan!')
+  let n = Number(split[0]) - 1
+  if (catatan[n] == undefined) return m.reply('Catatan tidak ditemukan!')
+  let tmp = []
 
-export default handler
+  for (let ct in catatan) {
+    if(ct != n) {
+      tmp.push(catatan[ct])
+    } else {
+      continue
+    }
+  }
+
+  cdang = global.db.data.users[m.sender].catatan
+  global.db.data.users[m.sender].catatan = tmp
+
+conn.reply(m.chat, `Berhasil menghapus catatan!`, m, false, {
+    contextInfo: {
+      mentionedJid: conn.parseMention(text)
+    }
+  })
+}
+
+handler.help = ['hapuscatatan title']
+handler.tags = ['internet']
+handler.command = /^hapuscatatan|hc$/i
+
+export default handler;
