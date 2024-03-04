8ad35cf8fab3b3ff31179ab48e69d22a6444c399
Tanaka <projectthenext1532@gmail.com>
2024-03-05 05:53:34 +0700

    Update fun-gay.js

--- plugins/fun-gay.js
+++ plugins/fun-gay.js
@@ -1,13 +1,47 @@
-let handler = async (m, { conn }) => {
-  let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-  let name = conn.getName(who)
-  conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/gay', {
-    avatar: await conn.profilePictureUrl(who, 'image').catch(_ => './src/avatar_contact.png'), 
-  }), 'gay.png', `🏳️‍🌈  *Gay :* ${name}\n\nSiapa yang ingin memperkosa gay ini? `, m)
+module.exports = {
+before: async function (m) {
+    this.autosholat = this.autosholat || {}
+    let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? this.user.jid : m.sender
+    let id = m.chat
+    let jadwalSholat = {
+            Fajr: "04:49",
+            Sunrise: "06:04",
+            Dhuhr: "12:06",
+            Asr: "15:21",
+            Sunset: "18:08",
+            Maghrib: "18:08",
+            Isha: "19:38",
+            Imsak: "04:39",
+            Midnight: "00:06",
+            Firstthird: "22:07",
+            Lastthird: "02:06"
+        }
+    const date = new Date((new Date).toLocaleString("en-US", {
+        timeZone: "Asia/Jakarta"
+    }));
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const timeNow = `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}`;
+    let isActive = Object.values(this.autosholat).includes(true);
+    if (id in this.autosholat && isActive) {
+        return false
+    }
+
+    for (const [sholat, waktu] of Object.entries(jadwalSholat)) {
+        if (timeNow === waktu && !(id in this.autosholat)) {
+            let caption = `oi ngaf @${who.split`@`[0]},\nWaktu *${sholat}* telah tiba, ambilah air wudhu dan segeralah shalat.\n\n*${waktu}*\n_untuk wilayah Jakarta dan sekitarnya._`
+            this.autosholat[id] = [
+                this.reply(m.chat, caption, null, {
+                    contextInfo: {
+                        mentionedJid: [who]
+                    }
+                }),
+                setTimeout(() => {
+                    delete this.autosholat[id]
+                }, 57000)
+            ]
+        }
+    }
+},
+disabled: false
 }
-
-handler.help = ['gay @user']
-handler.tags = ['fun']
-handler.command = ['gay'] 
-
-export default handler
