05c09161a5702533628e2cf105764eac6087ec5d
Tanaka <projectthenext1532@gmail.com>
2024-03-05 02:56:59 +0700

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -151,9 +151,9 @@ text: text,
 contextInfo: {
 externalAdReply: {
 title: 'ᴍᴇɴᴜ',
-body: "",
+body: esteh,
 thumbnailUrl: 'https://telegra.ph/file/287baf39c90619f2d046c.jpg',
-sourceUrl: 'https://m.mobilelegends.com/en',
+sourceUrl: 'ʙᴏᴛ_ᴡʜᴀᴛsᴀᴘᴘ',
 mediaType: 1,
 renderLargerThumbnail: false
 }}}, { quoted: m})
@@ -188,4 +188,4 @@ function clockString(ms) {
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
   return [d, 'd ', h, 'h ', m, 'm '].map(v => v.toString().padStart(2, 0)).join('')
-  }
+             }
