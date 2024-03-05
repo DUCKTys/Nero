58d3fc87506dcfdadfcbb3d114a1441222179816
Tanaka <projectthenext1532@gmail.com>
2024-03-05 15:26:18 +0700

    Update _expired-gp.js

--- plugins/_expired-gp.js
+++ plugins/_expired-gp.js
@@ -5,7 +5,7 @@ export async function all(m) {
     if (!chats.expired)
         return !0
     if (+new Date() > chats.expired) {
-        await this.reply(m.chat, `🔴 Selamat tinggal Shavals *${this.user.name}* akan meninggalkan grup \n\nSaya mengakhiri sewa Anda`)
+        await this.reply(m.chat, `🔴 Selamat tinggal *${this.user.name}* akan meninggalkan grup \n\nSaya mengakhiri sewa Anda`)
         await this.groupLeave(m.chat)
         chats.expired = null
     }
